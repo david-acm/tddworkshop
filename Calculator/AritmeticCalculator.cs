38766a98f3e4bee80b76ae4a68d5a482ba02a680
davidchaparro <davidacm7@outlook.com>
2023-12-20 11:27:24 -0500

    First commit

---
+++ Calculator/AritmeticCalculator.cs
@@ -0,0 +1,15 @@
+namespace TestDrivenDesignIntro; 
+
+public class AritmeticCalculator {
+  // Propiedades o Campos
+  
+  // Metodos
+  // (Modificador de accesibilidad) (Tipo de dato de respuesta) (Nombre) ((Parametros) (tipo )(Nombre))
+  // public | private | protected
+  // string | int | char | enums | float | bool | double 64 bits | long 128 | decimal 
+  // PascalCase
+  // camelCase
+  public int AddNumbers(int operator1, int operator2) {
+    return operator1 + operator2;
+  }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TestDrivenDesignIntro;
+
+Console.WriteLine("Hello, to my calculator!");
+
+Console.WriteLine("Please input the first number");
+var operator1 = Console.ReadLine();
+Console.WriteLine("Please input the second number");
+var operator2 = Console.ReadLine();
+
+Console.WriteLine("Your result is:");
+var calculator = new AritmeticCalculator();
+var result = calculator.AddNumbers(
+  Int32.Parse(operator1), 
+  Int32.Parse(operator2));
+
+Console.WriteLine(result);
